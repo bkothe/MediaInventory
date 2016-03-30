817609e6b47de2275fc0245493b8a519b84149cd
Brian Kothe <bkothe@reachmail.com>
2016-03-30 07:08:03 -0500

    Save

---
+++ src/MediaInventory.Tests/Acceptance/Ui/api/media/audio/AudioPutHandlerTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Net;
+using MediaInventory.Core.Artist;
+using MediaInventory.Core.Media;
+using MediaInventory.Tests.Acceptance.Common.Http;
+using MediaInventory.Tests.Common.Data.TestData;
+using MediaInventory.UI.api.media.audio;
+using NUnit.Framework;
+using Should;
+
+namespace MediaInventory.Tests.Acceptance.Ui.api.media.audio
+{
+    [TestFixture]
+    public class AudioPutHandlerTests
+    {
+        private const string PutAudioUrl = "api/media/audio/{0}";
+
+        private Artist ArtistModified;
+        private const string TitleModified = "Six Degrees Of Inner Turbulence";
+        private const MediaFormat MediaFormatModified = MediaFormat.Shn;
+        private readonly DateTime ReleasedModified = DateTime.Now.AddYears(-10).Date;
+        private readonly DateTime PurchasedModified = DateTime.Now.AddYears(-9).Date;
+        private const decimal PurchasePriceModified = 15M;
+        private const string PurchaseLocationModified = "Kiss The Sky";
+        private const int MediaCountModified = 6;
+        private const string NotesModified = "Another DT album.";
+
+        private UiAcceptanceTestData _testData;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _testData = TestData.ForAcceptanceTests().OnUi();
+            ArtistModified = _testData.Artists.Create().Artist;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _testData.CleanUp();
+        }
+
+        [Test]
+        public void should_modify_audio()
+        {
+            var audio = _testData.Audios.Create().Audio;
+
+            Http.ForUi.AsPublic(false, false, PutAudioUrl, audio.Id)
+                .PutJson(new AudioPutHandler.Request
+                {
+                    ArtistId = ArtistModified.Id,
+                    Title = TitleModified,
+                    MediaFormat = (int)MediaFormatModified,
+                    Released = ReleasedModified,
+                    Purchased = PurchasedModified,
+                    PurchasePrice = PurchasePriceModified,
+                    PurchaseLocation = PurchaseLocationModified,
+                    MediaCount = MediaCountModified,
+                    Notes = NotesModified
+                })
+                .Status.ShouldEqual(HttpStatusCode.OK);
+
+            _testData.Session.Refresh(audio);
+
+            audio.Artist.Id.ShouldEqual(ArtistModified.Id);
+            audio.Title.ShouldEqual(TitleModified);
+            audio.MediaFormat.ShouldEqual(MediaFormatModified);
+            audio.Released.ShouldEqual(ReleasedModified);
+            audio.Purchased.ShouldEqual(PurchasedModified);
+            audio.PurchasePrice.ShouldEqual(PurchasePriceModified);
+            audio.PurchaseLocation.ShouldEqual(PurchaseLocationModified);
+            audio.MediaCount.ShouldEqual(MediaCountModified);
+            audio.Notes.ShouldEqual(NotesModified);
+        }
+
+        [Test]
+        public void should_modify_audio_and_set_nullables()
+        {
+            var audio = _testData.Audios.Create(x =>
+            {
+                x.Released = DateTime.Now;
+                x.Purchased = DateTime.Now;
+                x.PurchasePrice = 15M;
+            }).Audio;
+
+            Http.ForUi.AsPublic(false, false, PutAudioUrl, audio.Id)
+                .PutJson(new AudioPutHandler.Request
+                {
+                    ArtistId = audio.Artist.Id,
+                    Title = audio.Title,
+                    MediaFormat = (int)audio.MediaFormat,
+                    Released = null,
+                    Purchased = null,
+                    PurchasePrice = null,
+                    MediaCount = audio.MediaCount
+                })
+                .Status.ShouldEqual(HttpStatusCode.OK);
+
+            _testData.Session.Refresh(audio);
+
+            audio.Released.ShouldBeNull();
+            audio.Purchased.ShouldBeNull();
+            audio.PurchasePrice.ShouldBeNull();
+        }
+
+        [Test]
+        public void should_return_not_found_when_artist_does_not_exist()
+        {
+            Http.ForUi.AsPublic(false, false, PutAudioUrl, _testData.Concerts.Create().Concert.Id)
+                .PutJson(new AudioPutHandler.Request
+                {
+                    ArtistId = Guid.NewGuid()
+                })
+                .Status.ShouldEqual(HttpStatusCode.NotFound);
+        }
+    }
+}
---
+++ src/MediaInventory.UI/api/media/audio/AudioPutHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using MediaInventory.Core.Artist;
+using MediaInventory.Core.Media;
+using MediaInventory.Infrastructure.Common.Collections;
+using MediaInventory.Infrastructure.Common.Data.Orm;
+
+namespace MediaInventory.UI.api.media.audio
+{
+    public class AudioPutHandler
+    {
+        private readonly AudioModificationService _audioModificationService;
+        private readonly IRepository<Artist> _artists;
+
+        public class Request
+        {
+            public Guid AudioId { get; set; }
+            public Guid ArtistId { get; set; }
+            public string Title { get; set; }
+            public int MediaFormat { get; set; }
+            public DateTime? Released { get; set; }
+            public DateTime? Purchased { get; set; }
+            public decimal? PurchasePrice { get; set; }
+            public string PurchaseLocation { get; set; }
+            public int MediaCount { get; set; }
+            public string Notes { get; set; }
+        }
+
+        public AudioPutHandler(AudioModificationService audioModificationService, IRepository<Artist> artists)
+        {
+            _audioModificationService = audioModificationService;
+            _artists = artists;
+        }
+
+        public void Execute_AudioId(Request request)
+        {
+            _audioModificationService.Modify(request.AudioId, x =>
+            {
+                x.Artist = _artists.FirstOrThrowNotFound(y => y.Id == request.ArtistId, request.ArtistId, "Artist");
+                x.Title = request.Title;
+                x.MediaFormat = (MediaFormat)request.MediaFormat;
+                x.Released = request.Released;
+                x.Purchased = request.Purchased;
+                x.PurchasePrice = request.PurchasePrice;
+                x.PurchaseLocation = request.PurchaseLocation;
+                x.MediaCount = request.MediaCount;
+                x.Notes = request.Notes;
+            });
+        }
+    }
+}
