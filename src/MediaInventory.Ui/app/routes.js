fca8f465d12e07003f133701d3b310ea3f2045f6
Brian Kothe <14959395+bkothe@users.noreply.github.com>
2017-10-01 11:26:53 -0500

    Removed js files

--- src/MediaInventory.Ui/app/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var dashboard_component_1 = require("./dashboard.component");
-exports.appRoutes = [
-    { path: 'dashboard', component: dashboard_component_1.DashboardComponent },
-    { path: 'artists', loadChildren: 'app/artist/artist.module#ArtistModule' },
-    { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
-];
-//# sourceMappingURL=routes.js.map
